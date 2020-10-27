fa10872c318b7d43a42511f937e46a257eba1ae4
Jake Taylor <jacobdeantaylor@gmail.com>
2020-10-26 18:07:56 -0700

    Added Like/Unlike Posts Feature

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -126,6 +126,54 @@ router.put('/like/:id', auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
 
+    // Check if the post has already been liked
+    // Compare current user to the user thats logged in - turn into string so that it matches the id in the req
+    // If the length is greater than 0, means it has already been liked (already a like in the req.user.id that matches the user attempting the like)
+    if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+      return res.status(400).json({ msg: 'Post already liked' });
+    }
+
+    // If the user hasnt already liked the post, take likes array and add the like to the beginning of the array
+    post.likes.unshift({ user: req.user.id });
+
+    // Save it back to the DB
+    await post.save();
+
+    res.json(post.likes);
+    
+    
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+
+// @route PUT api/posts/unlike/:id
+// @desc Unlike a post
+// @access Private
+router.put('/unlike/:id', auth, async (req, res) => {
+
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if the post has already been liked
+    // Compare current user to the user thats logged in - turn into string so that it matches the id in the req
+    // If the length is equal to 0, means post has not yet been liked
+    if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+      return res.status(400).json({ msg: 'Post has not yet been liked' });
+    }
+
+    // Get remove index
+    const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+
+    // Splice out of array
+    post.likes.splice(removeIndex, 1);
+
+    // Save it back to the DB
+    await post.save();
+
+    res.json(post.likes);
     
     
   } catch (err) {
